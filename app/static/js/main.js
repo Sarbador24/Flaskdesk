297822464cff8019bfaa603f428ec2cff465c41b
hazzillrodriguez <hazzillrodriguez@gmail.com>
2021-11-27 11:38:49 +0800

    Refactor admin with WTForms macros

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -2,4 +2,16 @@ $(document).ready(function() {
   $('#table').DataTable({
     'ordering': false
   });
+
+  new FroalaEditor('textarea#body, textarea#comment', {
+    toolbarButtons: ['fontFamily', '|', 'bold', 'italic', 'underline', 'undo', 'redo', 'codeView'],
+    fontFamilySelection: true,
+    pluginsEnabled: ['fontFamily'],
+    heightMin: 100,
+    heightMax: 200
+  });
+  
+  new PerfectScrollbar(".list-scrollbar");
+  var nanobar = new Nanobar();
+  nanobar.go(100);
 });
